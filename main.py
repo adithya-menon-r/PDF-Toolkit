b18b9665fbf3dec1ade60953f7c4077c09f89030
Adithya Menon R <adukottan@gmail.com>
2025-06-02 04:08:58 +0530

    Integrated a custom TUI

--- main.py
+++ main.py
@@ -1,59 +1,65 @@
 import os
-from tabulate import tabulate
-from termcolor import colored
+import sys
+import shutil
+from rich import print as printf
 
+from pdf_tools import PDFTools
+from tui import SelectionMenu, ReorderMenu
 from file_dialogs import select_files_dialog, save_file_dialog
-from utils import display_file_order, reorder_files
-from pdf_tools import PDFMerger
+
+TOOL_OPTIONS = ["Merge PDFs", "Exit"]
+
 
 def clear_screen():
-    os.system('cls')
-    print("-----------------------------------------------------------------------------------------------------------")
-    print("PDF MERGER")
-    print("-----------------------------------------------------------------------------------------------------------")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    terminal_width = shutil.get_terminal_size((80, 20)).columns
+    title = "PDF Toolkit"
+    centered_title = title.center(terminal_width)
+    printf(f"[bold #FFAA66  ]{centered_title}[/bold #FFAA66  ]")
+    underline = "─" * terminal_width
+    printf(f"[#FFECB3]{underline}[/#FFECB3]")
+
 
 def main():
     try:
         clear_screen()
-        merger = PDFMerger()
+        tool = PDFTools()
 
-        while True:
-            print(tabulate([["1", "Merge PDFs"]], tablefmt="rounded_grid"))
-            select_tool = input("Choose the tool \n>> ").lower().strip()
-            if select_tool in ["1", "Merge  PDFs"]:
-                clear_screen()
+        menu_choice = SelectionMenu("Please select one of the tools:", TOOL_OPTIONS).run()
+
+        if menu_choice == "Merge PDFs":
+            while True:
                 files = select_files_dialog()
-                if files is None:
-                    print(colored(f"\nPROGRAM WAS TERMINATED.", "red", attrs=["bold"]))
-                    return
-                break
-            else:
-                print(colored(f"\nERROR: Invalid choice. Please choose between 1/2.\n", "red", attrs=["bold"]))
+                if files is not None:
+                    break
+                if SelectionMenu("Are you sure you want to cancel?", ["No", "Yes"]).run() == "Yes":
+                    sys.exit(1)
+
+        elif menu_choice == "Exit":
+            sys.exit(0)
+
+        files = ReorderMenu(
+            "Reorder the files if required: ",
+            files
+        ).run()
+
+        tool.merge(files)
 
         while True:
-            clear_screen()
-            print(f"The {len(files)} files selected for merging will be merged in the following order: ")
-            display_file_order(files)
-            reorder_confirm = input(f"Press {colored("R", "blue", attrs=["bold"])} to Reorder the files or {colored("Enter", "blue", attrs=["bold"])} to merge.\n>> ")
-            if reorder_confirm.lower().strip() in ["r", "reorder", "re", "y", "yes"]:
-                files = reorder_files(files)
-            else:
+            save_path = save_file_dialog()
+            if save_path is not None:
                 break
-
-        merger.merge(files)
-
-        save_path = save_file_dialog()
-        if save_path is None:
-            cancel_confirm = input(f"No Save Location was selected. Press {colored("R", "blue", attrs=["bold"])} to Retry.\n>> ")
-            if cancel_confirm.lower().strip() in ["r", "retry"]:
-                save_path = save_file_dialog()
+            if SelectionMenu("Are you sure you want to cancel?", ["No", "Yes"]).run() == "Yes":
+                sys.exit(1)
         
-        if save_path is not None:
-            merger.export(save_path)
-            print(colored(f"SUCCESS! The Merged PDF File has been saved to {save_path}", "green", attrs=["bold"]))
+        tool.export(save_path)
+        printf(f"[#A3BE8C]✔[/#A3BE8C] [bold #FFD580]Merged PDF saved to {save_path}")
 
     except KeyboardInterrupt:
-        print(colored(f"\nPROGRAM WAS TERMINATED due to KeyboardInterrupt!\n", "red", attrs=["bold"]))
+        printf(f"[bold red]PROGRAM WAS TERMINATED due to KeyboardInterrupt!\n")
+
+    finally:
+        printf(f"[bold #A3BE8C]Goodbye!")
 
 
 if __name__ == "__main__":
--- pdf_tools.py
+++ pdf_tools.py
@@ -1,18 +1,21 @@
 from io import BytesIO
 from pathlib import Path
 from pypdf import PdfWriter
-from termcolor import colored
+from tui import ProgressBar
 
-class PDFMerger:
+class PDFTools:
     def __init__(self):
         self.generated_file = None
 
     def merge(self, files):
         writer = PdfWriter()
-        print("\nWorking on it...")
-        for i, pdf in enumerate(files):
+
+        def process_merge(pdf):
             writer.append(pdf)
-            print(colored(f"{i+1} Done!", "blue", attrs=["bold"]))
+
+        progress = ProgressBar("Merging PDFs", files)
+        progress.run(process_merge)
+
         self.generated_file = BytesIO()
         writer.write(self.generated_file)
         self.generated_file.seek(0)
