73625fe08a4ba0f614e1219ac88cfb27f6b3e4d0
Adithya Menon R <adukottan@gmail.com>
2025-06-04 00:54:15 +0530

    Added about option in TUI

--- pdfwerks/tui_interface.py
+++ pdfwerks/tui_interface.py
@@ -8,7 +8,7 @@ from .pdf_tools import PDFTools
 from .tui import SelectionMenu, ReorderMenu
 from .utils import get_files, get_save_path
 
-OPTIONS = ["Merge PDFs", "Compress PDF", "Exit"]
+OPTIONS = ["Merge PDFs", "Compress PDF", "About", "Exit"]
 
 
 def clear_screen():
@@ -58,6 +58,26 @@ def run_tui():
             tool.export(save_path)
             pyperclip.copy(save_path)
             printf("[#A3BE8C]✔[/#A3BE8C] [bold #FFD580] Compressed PDF saved!\n[/bold #FFD580]")
+
+        elif menu_choice == "About":
+            clear_screen()
+            about_text = "\n".join([
+                "[bold #FFAA66]PDFwerks[/bold #FFAA66] is a lightweight Python toolkit that offers simple, fast, and private PDF manipulation tools, all running locally.",
+                "",
+                "[bold #FFD580]✓ Merge PDFs[/bold #FFD580]",
+                "[bold #FFD580]✓ Compress PDFs[/bold #FFD580]",
+                "[dim]More tools coming soon![/dim]",
+                "",
+                "PDFwerks also supports CLI for quick operations - run [bold]pdfwerks --help[/bold] or check the docs to learn more.",
+                "",
+                "[bold #FFAA66]Developers & Contributions:[/bold #FFAA66]",
+                "PDFwerks is open source and welcomes contributions!",
+                "Repo: [link=https://github.com/adithya-menon-r/PDFwerks]adithya-menon-r/PDFwerks[/link]",
+                "",
+                "[bold #FFAA66]Author:[/bold #FFAA66] Adithya Menon R\n",
+            ])
+
+            printf(about_text)
 
         elif menu_choice == "Exit":
             sys.exit(0)
